a64749846808eb86eed930696362972e104d0642
Bryan-CoderS <139633909+Bryan-CoderS@users.noreply.github.com>
2024-12-25 01:52:06 +0800

    Star
    + added initial rotation to suit the code
    + added function to the star
    - bug remains

--- src/game_objects/star/star.gd
+++ src/game_objects/star/star.gd
@@ -1,2 +1,40 @@
 extends Node2D
 class_name Star
+
+@onready var timer = $Timer
+var drag_mode: bool = true #drag mode on default
+var touching_mouse: bool = false
+var holding: bool = false
+
+func _process(delta):
+	if holding:
+		if drag_mode:
+			global_position = get_global_mouse_position()
+		else:
+			look_at(get_global_mouse_position()) ## problem: it can't be paused with releasing left click
+
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and touching_mouse: 
+			if event.is_pressed(): #to define hold
+				holding = true
+				timer.start()
+			else:
+				if timer.time_left != 0: #within time
+					clicked()
+				timer.stop()
+				holding = false
+
+func clicked():
+	if touching_mouse: #inside star
+		if drag_mode: 
+			drag_mode = false
+			print("angle")
+		else:
+			drag_mode = true
+			print("drag")
+
+func _on_area_2d_mouse_entered():
+	touching_mouse = true
+
+func _on_area_2d_mouse_exited():
+	touching_mouse = false

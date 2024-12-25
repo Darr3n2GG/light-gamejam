e36cd12c1cc1790693fd9727a2c1a1267b2266b2
Bryan-CoderS <139633909+Bryan-CoderS@users.noreply.github.com>
2024-12-25 18:30:14 +0800

    Star Mode Update
    + revert to drag mode after unselect
    + mode changes colour
    + angle only change when cursor is outside the star
    + new png file for star

--- src/game_objects/star/star.gd
+++ src/game_objects/star/star.gd
@@ -2,6 +2,9 @@ extends Node2D
 class_name Star
 
 @onready var timer = $Timer
+@onready var drag = $DragMode
+@onready var angle = $AngleMode
+
 var drag_mode: bool = true
 var touching_mouse: bool = false
 var holding: bool = false
@@ -10,16 +13,20 @@ func _process(delta):
 	if holding:
 		if drag_mode:
 			global_position = get_global_mouse_position()
-		else:
+		elif !touching_mouse:
 			rotate(get_angle_to(get_global_mouse_position())+1.5708)
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT: 
-		if event.is_pressed() and touching_mouse: #to define hold
-			holding = true
-			timer.start()
+		if event.is_pressed():
+			if touching_mouse: #to define hold
+				holding = true
+				timer.start()
+			else: #revert mode after unselect
+				drag_mode = true
+		
 		if !event.is_pressed():
-			if timer.time_left != 0: #within time
+			if timer.time_left > 0: #within time
 				clicked()
 			timer.stop()
 			holding = false
@@ -28,8 +35,12 @@ func clicked():
 	if touching_mouse: #inside star
 		if drag_mode: 
 			drag_mode = false
+			angle.visible = true
+			drag.visible = false
 		else:
 			drag_mode = true
+			angle.visible = false
+			drag.visible = true
 
 func _on_area_2d_mouse_entered():
 	touching_mouse = true

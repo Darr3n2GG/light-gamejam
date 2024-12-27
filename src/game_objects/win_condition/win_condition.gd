5b1ec69ecb588b2f6cd5969e72fc44f7118c00b1
Bryan-CoderS <139633909+Bryan-CoderS@users.noreply.github.com>
2024-12-27 23:32:01 +0800

    Win condition update
    + Code to switch scene
    + Comment on light source
    + Changed file path for level
    + Clean scene level_2

--- src/game_objects/win_condition/win_condition.gd
+++ src/game_objects/win_condition/win_condition.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
 
-func win() -> void:
+func light_entered():
 	pass
+
+func win() -> void: #switch scene using code in Utilities
+	var current_scene_file = get_parent().scene_file_path
+	var next_level_number = current_scene_file.to_int() + 1
+	var next_level_name = "level_" + str(next_level_number)
+	Utilities.switch_scene(next_level_name, get_parent())

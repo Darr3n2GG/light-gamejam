184327dad5bd5782bd2831d1c1d55385a86796a4
Darr3n <darrensoonyuchien@gmail.com>
2024-12-27 23:24:02 +0800

    aaaaaa

--- src/game_objects/light_beam/light_beam.gd
+++ src/game_objects/light_beam/light_beam.gd
@@ -11,14 +11,15 @@ func _process(_delta: float) -> void:
 	var cast_point = target_position
 	
 	if self.is_colliding():
-		cast_point = self.get_collision_point()
+		cast_point = get_collision_point()
 		var collider = get_collider()
+		print("colliding point: " + str(cast_point) + " , collider pos: " + str(collider.global_position))
 		star_detecter.detect_star(collider)
 	else:
 		if star_detecter.check_collided_star_light_enabled():
 			star_detecter.hide_light_of_star()
 	#sprite.region_rect.end.x = end_point.position.length()
-	$Line2D.points[1] = cast_point
+	$Line2D.set_point_position(1, cast_point)
 	
 func enable_light_beam() -> void:
 	light_beam_enabled = true
@@ -28,5 +29,5 @@ func enable_light_beam() -> void:
 func disable_light_beam() -> void:
 	light_beam_enabled = false
 	self.target_position.y = 0
-	self.enabled = false	
+	self.enabled = false
 	self.hide()

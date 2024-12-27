64ec366274745b9e32f394476e306e14fb59686d
Darr3n <darrensoonyuchien@gmail.com>
2024-12-27 23:48:33 +0800

    debug 3

--- src/game_objects/light_beam/light_beam.gd
+++ src/game_objects/light_beam/light_beam.gd
@@ -1,25 +1,19 @@
 extends RayCast2D
 class_name LightBeam
 
-@onready var sprite = $Sprite
-@onready var end_point = $EndPoint
 var star_detecter = StarDetecter.new()
 
 var light_beam_enabled : bool = false
 
 func _process(_delta: float) -> void:
-	var cast_point = target_position
-	
+	var cast_point = target_position.x
 	if self.is_colliding():
-		cast_point = get_collision_point()
+		cast_point = get_collision_point().length()
 		var collider = get_collider()
-		print("colliding point: " + str(cast_point) + " , collider pos: " + str(collider.global_position))
 		star_detecter.detect_star(collider)
 	else:
-		if star_detecter.check_collided_star_light_enabled():
-			star_detecter.hide_light_of_star()
-	#sprite.region_rect.end.x = end_point.position.length()
-	$Line2D.set_point_position(1, cast_point)
+		star_detecter.hide_light_of_star()
+	$Line2D.points[1].x = cast_point
 	
 func enable_light_beam() -> void:
 	light_beam_enabled = true

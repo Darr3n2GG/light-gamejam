99458b45dea3d98408c528fda91b14c66ece7305
Darr3n <darrensoonyuchien@gmail.com>
2024-12-28 15:47:28 +0800

    added template level

--- src/game_objects/light_beam/light_beam.gd
+++ src/game_objects/light_beam/light_beam.gd
@@ -8,7 +8,7 @@ var light_beam_enabled : bool = false
 func _process(_delta: float) -> void:
 	var cast_point = target_position.x
 	if self.is_colliding():
-		cast_point = get_collision_point().length()
+		cast_point = (get_collision_point() - global_position).length()
 		var collider = get_collider()
 		star_detecter.detect_star(collider)
 	else:
--- src/game_objects/win_condition/win_condition.gd
+++ src/game_objects/win_condition/win_condition.gd
@@ -1,10 +1,10 @@
-extends Node2D
-
+extends Area2D
+class_name WinCondition
 
 func light_entered():
 	pass
 
-func win() -> void: #switch scene using code in Utilities
+func win() -> void:
 	var current_scene_file = get_parent().scene_file_path
 	var next_level_number = current_scene_file.to_int() + 1
 	var next_level_name = "level_" + str(next_level_number)

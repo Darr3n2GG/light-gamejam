f1d77de00fcef01794165cbbc17271ca94cbd3ab
Bryan-CoderS <139633909+Bryan-CoderS@users.noreply.github.com>
2024-12-29 00:36:52 +0800

    More levels

--- src/game_objects/win_condition/win_condition.gd
+++ src/game_objects/win_condition/win_condition.gd
@@ -17,8 +17,10 @@ func win() -> void:
 		
 func next_level() -> void:
 	var current_scene_file = get_parent().scene_file_path
-	var next_level_number = current_scene_file.left(1).to_int() + 1
+	var next_level_number = current_scene_file.to_int() + 1
 	var next_level_name = "level_" + str(next_level_number)
+	print(next_level_number)
+	print(next_level_name)
 	Utilities.switch_scene(next_level_name, get_parent())
 
 func _on_timer_timeout() -> void:
